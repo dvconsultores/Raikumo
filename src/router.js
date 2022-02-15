529a01258b9e3a2459e4935c7ca4de3e8cf89150
detextre4 <detextre4@gmail.com>
2022-02-15 19:19:53 -0400

    not Founf page

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Index from "./views/Index.vue";
 import MainNavbar from "./layout/MainNavbar.vue";
 import MainFooter from "./layout/MainFooter.vue";
+import NotFound from "./views/404.vue";
 
 Vue.use(Router);
 
@@ -16,7 +17,44 @@ export default new Router({
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
       }
-    }
+    },
+    {
+      path: "/history",
+      name: "index",
+      components: { default: Index, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" }
+      }
+    },
+    {
+      path: "/team",
+      name: "index",
+      components: { default: Index, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" }
+      }
+    },
+    {
+      path: "/FAQ",
+      name: "index",
+      components: { default: Index, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" }
+      }
+    },
+    {
+      path: "/jobs",
+      name: "index",
+      components: { default: Index, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" }
+      }
+    },
+    { path: "*", component: NotFound }
   ],
   scrollBehavior: to => {
     if (to.hash) {
---
+++ src/views/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="notFound"><h1 class="letras">(404) Page NotFound</h1><div class="containerMono"><img :src="Mono" alt="Monkey"/></div></div>
+</template>
+
+<script>
+export default{props:{mono:{type:String,default:require("@/assets/team/monkey.jpg")},},computed:{Mono(){return `${this.mono}`;},},};
+</script>
+
+<style scoped>
+.notFound{display:flex;margin:auto;justify-content:center;align-items:center;height:100vh;overflow:hidden}.letras{background:linear-gradient(10deg,#000 10%,rgba(83,202,251,.5),#fff);background-size:cover;background-clip:text;-webkit-background-clip:text;color:transparent;font-family:-apple-system,BlinkMacSystemFont,Arial,sans-serif !important;font-weight:800;text-align:center;z-index:1}.containerMono{position:absolute;width:40vmax;height:40vmax;border-radius:60%;overflow:hidden;z-index:0;animation:monito 5s infinite}.containerMono img{background:center no-repeat;background-size:cover}@keyframes monito{0%{transform:translateY(-50px)}30%{transform:translateY(0px) rotate(5deg)}50%{transform:rotate(-5deg)}75%{transform:rotate(5deg)}100%{transform:translateY(-50px)}}
+</style>
