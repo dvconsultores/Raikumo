960148638f3afba26078bca5372961311f6fa52d
detextre4 <detextre4@gmail.com>
2022-03-10 14:28:41 -0400

    update

--- src/views/sections/Team.vue
+++ src/views/sections/Team.vue
@@ -9,7 +9,7 @@
       v-for="(item, index) in dataTeam"
       v-bind:key="index"
     >
-      <img :class="Perfil" />
+      <img class="perfil" :src="item.perfil" alt="profile" />
     </aside>
   </section>
   <!-- <i class="fab fa-twitter" /> -->
@@ -24,144 +24,165 @@ export default {
           index: 0,
           nombre: "Endgame",
           twitter: "@endygamie",
-          cargo: "Founder"
+          cargo: "Founder",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 1,
           nombre: "Tyler Scott Ward",
           twitter: "@LordTylerWard",
-          cargo: "Advisor"
+          cargo: "Advisor",
+          perfil: require("@/assets/team/monkey.jpg")
         },
-        { index: 2, nombre: "Vlad", twitter: "@vgrichina", cargo: "Advisor" },
+        {
+          index: 2,
+          nombre: "Vlad",
+          twitter: "@vgrichina",
+          cargo: "Advisor",
+          perfil: require("@/assets/team/monkey.jpg")
+        },
         {
           index: 3,
           nombre: "Mark Ward",
           twitter: "@markwardbro",
-          cargo: "Advisor"
+          cargo: "Advisor",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 4,
           nombre: "Sharky",
           twitter: "@sharkyayzip",
-          cargo: "Story writer"
+          cargo: "Story writer",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 5,
           nombre: "Algy",
           twitter: "@youngalgy",
-          cargo: "Story writer"
+          cargo: "Story writer",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 6,
           nombre: "ikhimaz",
           twitter: "@ikhimaz",
-          cargo: "3D artist"
+          cargo: "3D artist",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 7,
           nombre: "Arki_bootzz",
           twitter: "@arki_bootzz",
-          cargo: "3D artist"
+          cargo: "3D artist",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 8,
           nombre: "Kuureh",
           twitter: "@kuureh_near",
-          cargo: "2D artist"
+          cargo: "2D artist",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 9,
           nombre: "fluffycookie",
           twitter: "@cookie_uw0",
-          cargo: "2D artist"
+          cargo: "2D artist",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 10,
           nombre: "Reyzlyn",
           twitter: "@Reyzlyn1",
-          cargo: "2D artist"
+          cargo: "2D artist",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 11,
           nombre: "Andresdom",
           twitter: "@andresdom2004",
-          cargo: "Developer"
+          cargo: "Developer",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 12,
           nombre: "detextre4",
           twitter: "@herian99611270",
-          cargo: "Developer"
+          cargo: "Developer",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 13,
           nombre: "Kuro",
           twitter: "@XLBao_",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 14,
           nombre: "Princemilfula",
           twitter: "@princemilfula",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 15,
           nombre: "Dustee",
           twitter: "@DUSTEEdarko",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 16,
           nombre: "DDCHR",
           twitter: "@DDCHR07",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 17,
           nombre: "Spike",
           twitter: "@candyfaguette",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 18,
           nombre: "Lanky",
           twitter: "@lanky_ay",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 19,
           nombre: "Starpause",
           twitter: "@starpause",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 20,
           nombre: "Nick Ward",
           twitter: "@nickawkward",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 21,
           nombre: "Zach",
           twitter: "@transmental",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         },
         {
           index: 22,
           nombre: "Illustrious",
           twitter: "illustriousboy",
-          cargo: "Contributors"
+          cargo: "Contributors",
+          perfil: require("@/assets/team/monkey.jpg")
         }
       ]
     };
-  },
-  computed: {
-    Perfil() {
-      return {
-        perfil: true
-      };
-    }
   }
 };
 </script>
@@ -183,7 +204,8 @@ export default {
   visibility: visible;
 }
 .perfil {
-  background: center no-repeat url("../../assets/team/monkey.jpg");
-  background-size: cover;
+  border-radius: 1000em;
+  width: 7.37625rem;
+  height: 7.37625rem;
 }
 </style>
