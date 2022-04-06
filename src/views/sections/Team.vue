df2364d6bc52f2fb8c3fa9bf54ebd362de146efd
detextre4 <detextre4@gmail.com>
2022-04-06 16:22:45 -0400

    twitter + texto

--- src/views/sections/Team.vue
+++ src/views/sections/Team.vue
@@ -28,161 +28,140 @@ export default {
     return {
       dataTeam: [
         {
-          index: 0,
           nombre: "Endgame",
           twitter: "@endygamie",
           cargo: "Founder",
           perfil: require("@/assets/team/endygamie.jpg")
         },
         {
-          index: 1,
           nombre: "Tyler Scott Ward",
           twitter: "@LordTylerWard",
           cargo: "Advisor",
           perfil: require("@/assets/team/LordTylerWard.jpg")
         },
         {
-          index: 2,
           nombre: "Vlad",
           twitter: "@vgrichina",
           cargo: "Advisor",
           perfil: require("@/assets/team/vlad.jpeg")
         },
         {
-          index: 3,
           nombre: "Mark Ward",
           twitter: "@markwardbro",
           cargo: "Advisor",
           perfil: require("@/assets/team/LordTylerWard.jpg")
         },
         {
-          index: 4,
           nombre: "Sharky",
           twitter: "@sharkyayzip",
           cargo: "Story writer",
           perfil: require("@/assets/team/shark.jpg")
         },
         {
-          index: 5,
           nombre: "Algy",
           twitter: "@youngalgy",
           cargo: "Story writer",
           perfil: require("@/assets/team/Algernon.jpg")
         },
         {
-          index: 6,
           nombre: "ikhimaz",
           twitter: "@ikhimaz",
           cargo: "3D artist",
           perfil: require("@/assets/team/ikhimaz.jpg")
         },
         {
-          index: 7,
           nombre: "Arki_bootzz",
           twitter: "@arki_bootzz",
           cargo: "3D artist",
           perfil: require("@/assets/team/arki.jpg")
         },
         {
-          index: 8,
           nombre: "Kuureh",
           twitter: "@kuureh_near",
           cargo: "2D artist",
           perfil: require("@/assets/team/kuureh.jpg")
         },
         {
-          index: 9,
           nombre: "fluffycookie",
           twitter: "@cookie_uw0",
           cargo: "2D artist",
           perfil: require("@/assets/team/Fluffycookie.jpg")
         },
         {
-          index: 10,
           nombre: "Reyzlyn",
           twitter: "@Reyzlyn1",
           cargo: "2D artist",
           perfil: require("@/assets/team/Reyzlyn.jpg")
         },
         {
-          index: 11,
           nombre: "Andresdom",
           twitter: "@andresdom2004",
           cargo: "Developer",
           perfil: require("@/assets/team/Andrés.jpg")
         },
         {
-          index: 12,
           nombre: "detextre4",
           twitter: "@herian99611270",
           cargo: "Developer",
           perfil: require("@/assets/team/herian.jpg")
         },
+        // {
+        //   index: 13,
+        //   nombre: "Kuro",
+        //   twitter: "@XLBao_",
+        //   cargo: "Contributors",
+        //   perfil: require("@/assets/team/monkey.jpg")
+        // },
         {
-          index: 13,
-          nombre: "Kuro",
-          twitter: "@XLBao_",
-          cargo: "Contributors",
-          perfil: require("@/assets/team/monkey.jpg")
-        },
-        {
-          index: 14,
           nombre: "Princemilfula",
           twitter: "@princemilfula",
           cargo: "Contributors",
           perfil: require("@/assets/team/princemilfula.jpg")
         },
+        // {
+        //   index: 15,
+        //   nombre: "Dustee",
+        //   twitter: "@DUSTEEdarko",
+        //   cargo: "Contributors",
+        //   perfil: require("@/assets/team/monkey.jpg")
+        // },
         {
-          index: 15,
-          nombre: "Dustee",
-          twitter: "@DUSTEEdarko",
-          cargo: "Contributors",
-          perfil: require("@/assets/team/monkey.jpg")
-        },
-        {
-          index: 16,
           nombre: "DDCHR",
           twitter: "@DDCHR07",
           cargo: "Contributors",
           perfil: require("@/assets/team/Totallynormie.jpg")
         },
         {
-          index: 17,
           nombre: "Spike",
           twitter: "@candyfaguette",
           cargo: "Contributors",
           perfil: require("@/assets/team/spike.jpg")
         },
         {
-          index: 18,
           nombre: "Lanky",
           twitter: "@lanky_ay",
           cargo: "Contributors",
           perfil: require("@/assets/team/lankwell.jpg")
         },
         {
-          index: 19,
           nombre: "Starpause",
           twitter: "@starpause",
           cargo: "Contributors",
           perfil: require("@/assets/team/starpause.jpg")
         },
         {
-          index: 20,
           nombre: "Nick Ward",
           twitter: "@nickawkward",
           cargo: "Contributors",
           perfil: require("@/assets/team/wardn.jpg")
         },
         {
-          index: 21,
           nombre: "Zach",
           twitter: "@transmental",
           cargo: "Contributors",
           perfil: require("@/assets/team/LordTylerWard.jpg")
         },
         {
-          index: 22,
           nombre: "Illustrious",
           twitter: "illustriousboy",
           cargo: "Contributors",
